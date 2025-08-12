f6c0ff7ab003a6ef6d03a416aa6963da4f9d1cc6
TrueVladimir <v.trusov@aerodisk.ru>
2025-08-12 15:50:05 +0300

    updates venv preparing

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ print_with_padding(){
 }
 
 prepare_venv(){
-    local create_docs_venv_command="uv --directory $DOCS_PROJECT_PATH venv -c"
+    local create_docs_venv_command="uv --directory $DOCS_PROJECT_PATH venv --seed"
     local install_requirements_command="uv sync --directory $DOCS_PROJECT_PATH"
 
     printf ">>>>>>Creating docs venv"
